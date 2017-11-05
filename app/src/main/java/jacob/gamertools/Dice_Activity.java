bf9fe1f0e2edb3c65f8bdbd7bb61c80b0bbe2198
Jacob <jrq5y3@mail.umkc.edu>
2017-11-05 06:17:36 -0600

    Stuff

--- app/src/main/java/jacob/gamertools/Dice_Activity.java
+++ app/src/main/java/jacob/gamertools/Dice_Activity.java
@@ -23,36 +23,48 @@ public class Dice_Activity extends AppCompatActivity {
         int sides=6;  //default numbers
         int dice=1;
         int result=0;
-        int max=0;
+        int max=6;
+        boolean overflow=false;
         try {
             TextView Dice_Count = (TextView) findViewById(R.id.Dice_Number);
             if(Dice_Count.getText().length()>9)
-            {dice=1;}                                  //////////////
+            {
+                dice=1;
+                overflow=true;
+            }                                  //////////////
             else {
                 dice = Integer.parseInt(Dice_Count.getText().toString());
             }
             TextView Side_Count = (TextView) findViewById(R.id.Side_Number);
             if(Side_Count.getText().length()>9)
-            {sides=1;}                                ///////////////
+            {
+                sides=1;
+                overflow=true;
+            }                                ///////////////
             else {
                 sides = Integer.parseInt(Side_Count.getText().toString());
             }
 
-            if (dice != 0 && Math.abs(sides) > Math.abs(Long.MAX_VALUE / dice))
+            long longDice=((long) dice);
+            long longSides=((long) sides);
+            long test=longDice*longSides;
+            if (test>Integer.MAX_VALUE)
             {
-                throw new ArithmeticException();
-            }  //the highest possible dice roll is the number of dice times their highest possible value
-
+                overflow=true;
+            }
             else
                 max=dice*sides;
 
             Random rand = new Random(System.currentTimeMillis());  //RNG based on current Millisecond
             result = rand.nextInt(max) + dice;
         } catch(ArithmeticException e)
-        {}
+        {return;}
         TextView Roll_Result = (TextView) findViewById(R.id.Roll_Result); //show result
-
         Roll_Result.setText(result + ""); //convert number to string
+        if(overflow)
+        {
+            Roll_Result.setText("Error:Result too large.");
+        }
 
         TextView BackLog = (TextView) findViewById(R.id.BackLog_Text);
         String BackLog_Text= (String) BackLog.getText();
